f9821f297cdef39f37a825626c0057a89e821c62
AdnanRim <adnanrime@gmail.com>
2023-10-10 20:08:43 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void function1(void);
+int function2(int arg);
+void function3(char *str);
+/* ... */
+
+/* Prototype for _putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */
