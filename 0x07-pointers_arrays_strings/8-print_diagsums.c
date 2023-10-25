d146fcc6aef3b06c69b6858ba952f95136fedda1
AdnanRim <adnanrime@gmail.com>
2023-10-24 18:39:13 -0700

    print diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+int i, diagonal1 = 0, diagonal2 = 0;
+
+for (i = 0; i < size; i++)
+{
+diagonal1 += a[i * size + i];
+diagonal2 += a[i * size + size - i - 1];
+}
+
+printf("%d, %d\n", diagonal1, diagonal2);
+}
